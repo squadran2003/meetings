8c9ed52e2ce0478aa8e87838c6c48622f6669734
Andreas Cormack <andreas@passthekeys.co.uk>
2026-02-09 19:53:44 +0000

    fix backend url

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -e
 
-export BACKEND_URL="${BACKEND_URL:-backend:8000}"
 export LISTEN_PORT="${PORT:-8080}"
 
-envsubst '${BACKEND_URL} ${LISTEN_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${LISTEN_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
-echo "nginx: listening on port ${LISTEN_PORT}, proxying to ${BACKEND_URL}"
+echo "nginx: listening on port ${LISTEN_PORT}"
