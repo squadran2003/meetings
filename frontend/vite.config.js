a525846201f8dd7acad03e7dcc8b62020d9598e3
Andreas Cormack <andreas@passthekeys.co.uk>
2026-02-10 20:42:20 +0000

    fix peer connection issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const backendUrl = env.VITE_BACKEND_DEV_URL || 'http://localhost:8000'
 
   return {
-    plugins: [vue(), vuetify({ autoImport: true })],
-    define: {
-      global: 'globalThis',
-    },
+    plugins: [vue(), vuetify({ autoImport: true }), nodePolyfills()],
     server: {
       port: 3000,
       proxy: {
